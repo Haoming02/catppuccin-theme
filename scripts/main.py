b8dda9e57c44df114964e07f9c20a5f0e2461039
Haoming <hmstudy02@gmail.com>
2025-04-30 21:10:28 +0800

    burnt

--- scripts/main.py
+++ scripts/main.py
@@ -1,35 +1,9 @@
 from modules.script_callbacks import on_ui_settings
 from modules.shared import OptionInfo, opts
 
-import gradio as gr
 import shutil
 import re
 import os
-
-
-flavors: tuple[str] = (
-    "latte",
-    "frappe",
-    "macchiato",
-    "mocha",
-)
-
-accents: tuple[str] = (
-    "rosewater",
-    "flamingo",
-    "pink",
-    "mauve",
-    "red",
-    "maroon",
-    "peach",
-    "yellow",
-    "green",
-    "teal",
-    "sky",
-    "blue",
-    "sapphire",
-    "lavender",
-)
 
 script_path = os.path.normpath(os.path.dirname(os.path.dirname(__file__)))
 
@@ -49,8 +23,6 @@ def on_accent_change():
     with open(os.path.join(script_path, "style.css"), "w") as file:
         file.write(style)
 
-    on_waves_change()
-
 
 def on_flavor_change():
     shutil.copy(
@@ -61,40 +33,42 @@ def on_flavor_change():
     on_accent_change()
 
 
-def on_waves_change():
-    with open(os.path.join(script_path, "style.css"), "r+") as file:
-        style = file.read()
-
-    _edit = False
-    animated: bool = "gradio-app" in style
-    animating: bool = opts.oled_waves
-
-    if animated and (not animating):
-        style = style.split("body gradio-app")[0]
-        _edit = True
-
-    if (not animated) and animating:
-        with open(os.path.join(script_path, "flavors", "waves.css"), "r") as waves:
-            anim = waves.read()
-        style = f"{style}{anim}"
-        _edit = True
-
-    if not _edit:
-        return
-
-    with open(os.path.join(script_path, "style.css"), "w") as file:
-        file.write(style)
-
-
 def on_settings():
+    from gradio import Radio
+
     args = {"section": ("ctp", "Catppuccin Theme"), "category_id": "ui"}
+
+    flavors: tuple[str] = (
+        "latte",
+        "frappe",
+        "macchiato",
+        "mocha",
+        "burnt",
+    )
+
+    accents: tuple[str] = (
+        "rosewater",
+        "flamingo",
+        "pink",
+        "mauve",
+        "red",
+        "maroon",
+        "peach",
+        "yellow",
+        "green",
+        "teal",
+        "sky",
+        "blue",
+        "sapphire",
+        "lavender",
+    )
 
     opts.add_option(
         "ctp_flavor",
         OptionInfo(
             default="mocha",
             label="Catppuccin Flavor",
-            component=gr.Radio,
+            component=Radio,
             component_args={"choices": flavors},
             onchange=on_flavor_change,
             **args,
@@ -106,22 +80,11 @@ def on_settings():
         OptionInfo(
             default="maroon",
             label="Accent",
-            component=gr.Radio,
+            component=Radio,
             component_args={"choices": accents},
             onchange=on_accent_change,
             **args,
         ),
-    )
-
-    opts.add_option(
-        "oled_waves",
-        OptionInfo(
-            default=False,
-            label="Background Animation",
-            component=gr.Checkbox,
-            onchange=on_waves_change,
-            **args,
-        ).info("mainly for OLED monitor"),
     )
 
 
